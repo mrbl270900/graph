0c16b61da9db9393b157c19be04720594e55d487
mrbl270900 <90680832+mrbl270900@users.noreply.github.com>
2021-10-18 13:55:07 +0200

    graph

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        graph matrix = new graph();
+        //graph matrix = new graph();
+        /*
         matrix.setmatrix(7);
         matrix.addedge(0,1,1);
         matrix.addedge(0,2,5);
@@ -17,11 +18,41 @@ public class Main {
         matrix.addedge(4,3,3);
         matrix.addedge(4,6,4);
         matrix.addedge(4,5,3);
-        matrix.addedge(5,6,1);
+        matrix.addedge(5,6,1);*/
+
+        AdjacencyGraph MyGraph=new AdjacencyGraph();
+        Vertex a = new Vertex("0");
+        Vertex b = new Vertex("1");
+        Vertex c = new Vertex( "2");
+        Vertex d = new Vertex("3");
+        Vertex e = new Vertex("4");
+        Vertex f = new Vertex( "5");
+        Vertex g = new Vertex("6");
+        MyGraph.addVertex(a);
+        MyGraph.addVertex(b);
+        MyGraph.addVertex(c);
+        MyGraph.addVertex(d);
+        MyGraph.addVertex(e);
+        MyGraph.addVertex(f);
+        MyGraph.addVertex(g);
+        MyGraph.addEdge(a,b,1);
+        MyGraph.addEdge(a,c, 5);
+        MyGraph.addEdge(a,e,3);
+        MyGraph.addEdge(b,e,1);
+        MyGraph.addEdge(b,f,7);
+        MyGraph.addEdge(c,d,1);
+        MyGraph.addEdge(d,e,1);
+        MyGraph.addEdge(d,g, 1);
+        MyGraph.addEdge(e,c, 1);
+        MyGraph.addEdge(e,d,3);
+        MyGraph.addEdge(e,f, 3);
+        MyGraph.addEdge(e,g,4);
+        MyGraph.addEdge(f,g, 1);
+        MyGraph.PrintGraph();
 
         
-        matrix.printmatrix();
-        matrix.prim();
+        //matrix.printmatrix();
+        //matrix.prim();
 
     }
 }
